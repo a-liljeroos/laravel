9603ba543db397074d904afcf11d28c0f0d62c5b
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-02-12 11:41:36 +0200

    select input

---
+++ resources/js/Components/SelectInput.tsx
@@ -0,0 +1,51 @@
+import {
+    forwardRef,
+    SelectHTMLAttributes,
+    useEffect,
+    useImperativeHandle,
+    useRef,
+} from "react";
+
+export default forwardRef(function SelectInput(
+    {
+        className = "",
+        isFocused = false,
+        optionItems = [],
+        ...props
+    }: SelectHTMLAttributes<HTMLSelectElement> & {
+        isFocused?: boolean;
+        optionItems: { value: string; label: string }[];
+    },
+    ref
+) {
+    const localRef = useRef<HTMLSelectElement>(null);
+
+    useImperativeHandle(ref, () => ({
+        focus: () => localRef.current?.focus(),
+    }));
+
+    useEffect(() => {
+        if (isFocused) {
+            localRef.current?.focus();
+        }
+    }, []);
+
+    return (
+        <select
+            {...props}
+            className={
+                "border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm " +
+                className
+            }
+            ref={localRef}
+        >
+            {optionItems.map((item, index) => {
+                return (
+                    <option key={index} value={item.value}>
+                        {item.label}
+                    </option>
+                );
+            })}
+        </select>
+    );
+});
