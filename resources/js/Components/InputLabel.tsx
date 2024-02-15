69021cb68685d0707731d2996a393d504d03b308
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-02-15 19:22:47 +0200

    optional error message in InputLabel

--- resources/js/Components/InputLabel.tsx
+++ resources/js/Components/InputLabel.tsx
@@ -1,9 +1,29 @@
-import { LabelHTMLAttributes } from 'react';
+import { LabelHTMLAttributes } from "react";
 
-export default function InputLabel({ value, className = '', children, ...props }: LabelHTMLAttributes<HTMLLabelElement> & { value?: string }) {
+export default function InputLabel({
+    value,
+    className = "",
+    children,
+    error,
+    ...props
+}: LabelHTMLAttributes<HTMLLabelElement> & { value?: string; error?: string }) {
     return (
-        <label {...props} className={`block font-medium text-sm text-gray-700 ` + className}>
-            {value ? value : children}
+        <label
+            {...props}
+            className={
+                `block font-medium text-sm text-gray-700 mt-4 mb-1` + className
+            }
+        >
+            {value ? value : children}{" "}
+            <span
+                style={{
+                    color: "red",
+                    marginLeft: 10,
+                    textDecoration: "underline",
+                }}
+            >
+                {error}
+            </span>
         </label>
     );
 }
