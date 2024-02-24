e013c74b56eb0bc43632ad8039182cdb33fa0354
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-02-24 13:13:04 +0200

    create product endpoint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,5 +22,26 @@ class ProductController extends Controller
         return Inertia::render('Products/Add');
     }
 
+    public function create(Request $request)
+    {
+        Product::create($request->validate([
+            'tags' => ['json'],
+            'name' => ['required ','string'],
+            'description_short' => ['required','string'],
+            'description_long' =>[ 'required','string'],
+            'price' => ['required','numeric','max:999999.99','min:0.00'],
+            'quantity' => ['required','integer'],
+            'brand_id' => ['required','integer'],
+            'category_ids' => ['required','json'],
+            'image_urls' => ['required','json'],
+            'ean' => ['required', 'string'],
+            'warranty' => ['required','integer', 'max:255'],
+            'active' => ['required','boolean'],
+        ]));
+
+        return redirect()->route('products.add')->with('success', 'Product added.');
+    }
+
+
     
 }
