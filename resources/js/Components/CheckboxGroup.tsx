1b240d8e1f030a2bf9135c33cb5bff3479355bde
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-02-24 13:12:02 +0200

    reset CheckboxGroup

--- resources/js/Components/CheckboxGroup.tsx
+++ resources/js/Components/CheckboxGroup.tsx
@@ -1,22 +1,46 @@
-import { InputHTMLAttributes, useState, useEffect } from "react";
+import {
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useState,
+    InputHTMLAttributes,
+    Ref,
+} from "react";
 import "./CheckboxGroup.scss";
+
+export type CheckBoxGroupRef = {
+    reset: () => void;
+};
 
 type ICheckboxGroup = InputHTMLAttributes<HTMLInputElement> & {
     checkBoxes: { label: string; value: string; checked?: boolean }[];
     onChange: (checked: string[]) => void;
 };
 
-export default function CheckboxGroup({
-    className = "",
-    checkBoxes = [],
-    onChange,
-    ...props
-}: ICheckboxGroup) {
+export default forwardRef(function CheckboxGroup(
+    { className = "", checkBoxes = [], onChange, ...props }: ICheckboxGroup,
+    ref: Ref<CheckBoxGroupRef>
+) {
     const [checked, setChecked] = useState<string[]>([]);
+
+    useEffect(() => {
+        const checkedValues = checkBoxes
+            .filter((c) => c.checked)
+            .map((c) => c.value);
+        setChecked(checkedValues);
+    }, []);
 
     useEffect(() => {
         onChange(checked);
     }, [checked]);
+
+    useImperativeHandle(ref, () => ({
+        reset,
+    }));
+
+    const reset = () => {
+        setChecked([]);
+    };
 
     return (
         <fieldset className="checkbox-group-cont">
@@ -28,6 +52,7 @@ export default function CheckboxGroup({
                         type="checkbox"
                         value={checkBox.value}
                         name={checkBox.value}
+                        checked={checked.includes(checkBox.value)}
                         onChange={(e) => {
                             setChecked((prevChecked) => {
                                 if (e.target.checked) {
@@ -53,4 +78,4 @@ export default function CheckboxGroup({
             ))}
         </fieldset>
     );
-}
+});
