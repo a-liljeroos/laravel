c484b055d6007a2e73690c2a9cc1c78fe3b8260a
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-03-07 14:56:25 +0200

    product edit & add controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,15 +12,15 @@ class ProductController extends Controller
 {
 
     private $productSchema = [
-        'tags' => ['json'],
+        'tags' => ['array'],
         'name' => ['required ','string'],
         'description_short' => ['required','string'],
         'description_long' =>[ 'required','string'],
         'price' => ['required','numeric','max:999999.99','min:0.00'],
         'quantity' => ['required','integer'],
         'brand_id' => ['required','integer'],
-        'category_ids' => ['required','json'],
-        'image_urls' => ['required','json'],
+        'category_ids' => ['required','array'],
+        'image_urls' => ['required','array'],
         'ean' => ['required', 'string'],
         'warranty' => ['required','integer', 'max:255'],
         'active' => ['required','boolean'],
@@ -29,19 +30,42 @@ class ProductController extends Controller
     public function index()
     {
         return Inertia::render('Products/Index', [
-            'products' => Product::all(),
+            'products' => Product::all()
         ]);
     }
 
-    public function add()
+    public function add(Request $request)
     {
-        return Inertia::render('Products/Add');
+        if($request->method('post')){
+            $productId = $request->input('product_id');
+            $product = Product::find($productId);
+            return Inertia::render('Products/Add', [
+                'product' => $product,
+                'categories' => Category::all(),
+            ]);
+        }
+
+        $categories = Category::all();
+
+        return Inertia::render('Products/Add', [
+            'categories' => $categories,
+        ]);
+    }
+
+    public function edit (Request $request)
+    {
+        $request->validate(['product_id' => 'required|integer'])['product_id'];
+        $productId = $request->input('product_id');
+        $product = Product::find($productId);
+        if ($product) {
+            $product->update($request->validate($this->productSchema));
+            return redirect()->route('products.index')->with('success', 'Product updated.');
+        }
     }
 
     public function create(Request $request)
     {
         Product::create($request->validate($this->productSchema));
-
         return redirect()->route('products.add')->with('success', 'Product added.');
     }
 
